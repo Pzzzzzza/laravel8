05f4eb3e1384b4e04f6d48b1ca12ff0274925ec8
Jessadaporn Pannaphost <150641872+Pzzzzzza@users.noreply.github.com>
2023-11-20 12:02:25 +0700

    Initial commit

---
+++ resources/views/test/ant.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>gallery of Phatcharee 65122420030 </h1>
+<p1>Ant <a href="gallery/ant"><img style="width: 250px;" height="250px;" src="{{ $ant }}" alt="#"></a></p1>
+    </body>
+</html>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <h1>gallery of Phatcharee 65122420030 </h1>
+<p1>Ant <a href="gallery/ant"><img style="width: 250px;" height="250px;" src="{{ $ant }}" alt="#"></a></p1>
+<p1>Bird <a href="gallery/bird"><img style="width: 250px;" height="250px;" src="{{ $bird }}" alt="#"></a></p1>
+<p1>cat <a href="gallery/cat"><img style="width: 250px;" height="250px;" src="{{ $cat }}" alt="#"></a></p1>
+<p1>god <a href="gallery/god"><img style="width: 250px;" height="250px;" src="{{ $god }}" alt="#"></a></p1>
+<p1>spider <a href="gallery/spider"><img style="width: 250px;" height="250px;" src="{{ $spider }}" alt="#"></a></p1>
+    </body>
+</html>
